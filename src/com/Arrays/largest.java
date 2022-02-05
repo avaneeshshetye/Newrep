4323061161f7559d0f2cc2a01fe4f646c7bbebb2
avaneeshshetye <avaneesh1305@gmail.com>
2022-02-05 14:34:34 +0530

    new files

--- src/com/Arrays/largest.java
+++ src/com/Arrays/largest.java
@@ -4,11 +4,18 @@ public class largest {
     public static void main(String[] args) {
         int arr[] = {77,83,99,5,0,8};
 
-        int max = arr[0];
+
 
         for(int i=0;i<=arr.length;i++){
+            for(int j=i+1;j<=arr.length;j++){
+
+                }
+
+
+            }
+            }
 
 
         }
-    }
-}
+
+
---
+++ src/com/Arrays/patterns.java
@@ -0,0 +1,34 @@
+package com.Arrays;
+
+public class patterns {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+    }
+}

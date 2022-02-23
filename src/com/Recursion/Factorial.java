e2b81ecf01e048d59293cffbef430f80553a1f6e
avaneeshshetye <avaneesh1305@gmail.com>
2022-02-23 22:29:39 +0530

    Recursion

---
+++ src/com/Recursion/Factorial.java
@@ -0,0 +1,28 @@
+package com.Recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Enter number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Factorial is: " + n);
+        int answer = Recursion(n);
+        System.out.println(answer);
+    }
+
+    public static int Recursion(int n) {
+        int result = 1;
+        if (n == 1) {
+            return n;
+        }
+
+        else{
+                return n * Recursion(n - 1);
+            }
+
+
+        }
+    }
+
